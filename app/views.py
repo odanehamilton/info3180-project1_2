acfbb91ff448ebd8c0c2f48eed32fc183fccc838
Odane P. Hamilton <odane.hamilton@gmail.com>
2016-05-19 01:01:27 +0000

    Updated files

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from app import app
-from flask import render_template, request, redirect, url_for,jsonify,g,session
+from flask import render_template, request, redirect, url_for,jsonify,session
 from app import db
 
 from flask.ext.wtf import Form 
-from wtforms.fields import TextField, PasswordField, FileField, IntegerField, RadioField
+from wtforms.fields import TextField, IntegerField, RadioField #,FileField
 from wtforms import validators
 from wtforms.validators import Required
 from app.models import Myprofile
@@ -22,40 +22,9 @@ class ProfileForm(Form):
      #image = FileField('image', [validators.regexp('\w+\.jpg$')])
 
 
-
-
-
-@app.before_request
-def before_request():
-    g.user = current_user
-
-    
-    
-
 ###
 # Routing for your application.
 ### 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    usernames = Myprofile.query.all()
-    if request.method == 'POST':
-        for names in usernames:
-            if request.form['username'] == names.username and request.form['password'] == names.password:
-                session['logged_in'] = True
-                return redirect(url_for('profile_view', id_num=names.id_num))
-        else:
-            error = "Invalid login data"
-    return render_template('login.html', error=error)
-    
-    
-@app.route('/logout')
-def logout():
-    """Render website's logout page."""
-    session.pop('logged_in', None)
-    return redirect(url_for('home'))
-
-    
 
 @app.route('/')
 def home():
@@ -103,38 +72,9 @@ def profile_view(id_num):
     return render_template('profile_view.html',profile=profile)
 
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
